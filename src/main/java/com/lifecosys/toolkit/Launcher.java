79f82c3ca782e642e58d278f34b24971a634f6e0
Young.Gu <hyysguyang@gmail.com>
2012-11-27 14:09:03 +0800

    Init the Android project
    Init the Android project, completed.
    
    ----------------------Committed by Young Gu

---
+++ src/main/java/com/lifecosys/toolkit/Launcher.java
@@ -0,0 +1,16 @@
+package com.lifecosys.toolkit;
+
+import android.app.*;
+import android.os.*;
+
+/**
+ * @author Young Gu
+ * @version 1.0 11/19/12 3:48 PM
+ */
+public class Launcher extends Activity
+{
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+	}
+}
